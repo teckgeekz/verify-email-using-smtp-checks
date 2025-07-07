8133c15417dcba84d80a932c2602287cc3b02f2d
teckgeekz <jeoffrey.mathews@jmgcapital.net>
2025-07-07 10:09:55 +0530

    update Front hero

--- Leadfinder/apps/nextjs/src/config/site.ts
+++ Leadfinder/apps/nextjs/src/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Saasfly",
+  name: "Leadfinder",
   description: "We provide an easier way to build saas service in production",
   url: "https://github.com/saasfly/saasfly",
   ogImage: "",
