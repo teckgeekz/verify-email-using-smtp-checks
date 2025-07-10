4f6987bb390ab40b0ea935ca93d5f4477fbeeccc
teckgeekz <jeoffrey.mathews@jmgcapital.net>
2025-07-10 13:46:44 +0530

    update text

--- Leadfinder/apps/nextjs/src/components/code-copy.tsx
+++ Leadfinder/apps/nextjs/src/components/code-copy.tsx
@@ -5,7 +5,7 @@ import * as Icons from "@saasfly/ui/icons";
 
 export function CodeCopy() {
   const [copied, setCopied] = useState(false)
-  const command = "bun create saasfly"
+  const command = "Email => Copy Clipboard"
 
   const copyToClipboard = async () => {
     try {
